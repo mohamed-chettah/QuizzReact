7c1506aff9ce6aa3dfaa6f0f0bda8398f6014a9a
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-02-10 14:54:50 +0100

    fix : design

--- quizz-react/src/components/Ui/PlayerCircle.tsx
+++ quizz-react/src/components/Ui/PlayerCircle.tsx
@@ -9,7 +9,7 @@ function PlayerCircle(props: { player: Player; reverse: boolean, isPlayer2?: boo
         <div
             className={`flex text-white gap-4 ${props.reverse ? "flex-row-reverse" : "flex-row"}`}
         >
-            <img className="w-12 h-12 rounded-full object-cover" src={props.player.image} alt={props.player.name} />
+            <img className="sm:w-12 w-8 sm:h-12 h-8 rounded-full object-cover" src={props.player.image} alt={props.player.name} />
             <div className="flex flex-col items-start gap-1">
                 <div className="player-circle__name sm:text-sm text-xs">{props.player.name}</div>
                 <div className={`text-yellow-300 text-xl ${props.isPlayer2 ? "score-player2" : "score-player1"}`}>{props.player.score}</div>
